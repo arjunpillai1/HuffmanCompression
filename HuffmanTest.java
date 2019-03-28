0dba0eb175253cb5a39cb30f5238f85ae9426116
arjunpillai1 <forgames4812@hotmail.com>
2019-03-28 02:47:01 -0400

    Add files via upload

--- HuffmanTest.java
+++ HuffmanTest.java
@@ -4,18 +4,20 @@ import java.io.IOException;
 public class HuffmanTest {
 
     public static void main(String[] args) throws IOException {
-
+        HuffmanTree tree =new HuffmanTree();
         FileInputStream in = null;
         FileOutputStream out = null;
 
+
         try {
-            in = new FileInputStream("ZeroImageSampleImage69.JPG");
+            in = new FileInputStream("hello.txt");
             out = new FileOutputStream("goodbye.txt");
             int c;
 
 
 
             while ((c = in.read()) != -1) {
+                tree.incrementCharacterFrequencies(c)
                 System.out.print(c);
                 System.out.println((char)c);
                 out.write(c);
@@ -34,20 +36,63 @@ public class HuffmanTest {
     public class HuffmanTree{
         HuffmanNode root;
 
+        private int[] possibleCharacter = new int[256];
+        private int[] characterFrequencies = new int[256];
+
+        HuffmanTree(){
+            for (int i=0;i<256;i++){
+                possibleCharacter[i]=i;
+            }
+        }
         /*
         build tree method
         dequeue the 2 lowest frequency nodes from priority queue and create a new node that has them as children
         add the new Node back into queue
         */
+        // after a list of
+        public void buildTree(){
+            PriorityQueue priorityQ= new PriorityQueue();
 
+            //initialise priority queue
+            for (int i=0;i<256;i++){
+                if (getCharacterFrequency(i)>=1){ // only create the node if the character exists
+                    priorityQ.enqueue(new HuffmanNode(getCharacter(i),getCharacterFrequency(i),null,null));
+                }
+            }
 
+            while (priorityQ.getSize()>1){
+                HuffmanNode leftChild =priorityQ.dequeue();
+                HuffmanNode rightChild=priorityQ.dequeue();
+                HuffmanNode parent = new HuffmanNode(-1,leftChild.getFrequency()+rightChild.getFrequency(),leftChild,rightChild)
+            }
+
+        }
+
+        public int getCharacterFrequency(int index) {
+            return characterFrequencies[index];
+        }
+
+        public void incrementCharacterFrequencies(int index) {
+            characterFrequencies[index]++;
+        }
+
+        public int getCharacter(int index) {
+            return possibleCharacter[index];
+        }
+
+        public void setPossibleCharacter(int[] possibleCharacter) {
+            this.possibleCharacter = possibleCharacter;
+        }
     }
+
     public class HuffmanNode implements Comparable<HuffmanNode>{
+        int value
         int frequency;
         HuffmanNode left=null;
         HuffmanNode right=null;
 
-        HuffmanNode(int frequency,HuffmanNode leftChild,HuffmanNode rightChild){
+        HuffmanNode(int value, int frequency,HuffmanNode leftChild,HuffmanNode rightChild){
+            this.value=value;
             this.frequency=frequency;
             left=leftChild;
             right=rightChild;
@@ -85,16 +130,125 @@ public class HuffmanTest {
             }
         }
     }
-    public class PriorityQueue<HuffmanNode> {
-        HuffmanTest.HuffmanNode
+
+    public class PriorityQueue {
+        PriorityNode head;
+        PriorityNode tail;
         int size;
-        int frequency;
+        //int frequency;
+
+        PriorityQueue(){
+            head=null;
+            tail=null;
+        }
 
         public void enqueue(HuffmanNode node){
-            frequency= node.getFrequency();
+            PriorityNode newNode = new PriorityNode(node);
+            if (head==null){ //when adding to an empty queue
+                head=newNode;
+                tail=head;
+            }else if (head==tail){ // if there's only one item in the queue (it's both the end and beginning)
+                if (newNode.compareTo(head) == -1){// if the new node has lower priority than the head
+                    PriorityNode tempNode = head;
+                    newNode.setNext(tempNode);
+                    tempNode.setNext(null);
+                    tempNode.setPrevious(newNode);
+                    tail=tempNode;
+                    head=newNode;
+                } else {
+                    newNode.setPrevious(head);
+                    newNode.setNext(null);
+                    head.setPrevious(null);
+                    head.setNext(newNode);
+                    tail = newNode;
+                }
+            }else {// this is the most common case, 2+ items in queue
+                PriorityNode tempNode =tail; // the moving node starting at the tail;
+                if (newNode.compareTo(tempNode) != -1){
+                    tempNode.setNext(newNode);
+                    newNode.setPrevious(tempNode);
+                    tail=newNode;
+                }
+                while (newNode.compareTo(tempNode)==-1){
+                    tempNode=tempNode.getPrevious();// this traverses through the queue
+
+                    if (newNode.compareTo(tempNode)!=-1){
+                        newNode.setNext(tempNode.getNext());
+                        newNode.setPrevious((tempNode));
+                        tempNode.setNext(newNode);
+                        tempNode.getNext().setPrevious(newNode);
+                    }
+                }
+            }
         }
         public HuffmanNode dequeue(){
-            return  null; // return the item with the lowest frequency
+            if (head==null) { // when there are no items
+                return null;
+            }else {
+                HuffmanNode temp =head.getItem(); // store the item before erasing the node
+                if (head.getNext()==null){
+                    head=null;
+                    return temp;
+                } else {
+                    head=head.getNext();
+                    if (head.getNext()==null){ //
+                        tail=null;
+                    }
+                    return temp;
+                }
+            }
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        private class PriorityNode implements Comparable<PriorityNode>{
+            HuffmanNode item;
+            PriorityNode next;
+            PriorityNode previous;
+            int priority;
+
+            public PriorityNode(HuffmanNode node) {
+                this.item = node;
+                this.priority = node.getFrequency();
+                this.next = null;
+                this.previous = null;
+            }
+
+            public PriorityNode getNext() {
+                return next;
+            }
+
+            public PriorityNode getPrevious() {
+                return previous;
+            }
+
+            public HuffmanNode getItem() {
+                return item;
+            }
+
+            public int getPriority() {
+                return priority;
+            }
+
+            public void setNext(PriorityNode next) {
+                this.next = next;
+            }
+
+            public void setPrevious(PriorityNode previous) {
+                this.previous = previous;
+            }
+            public int compareTo (PriorityNode node){
+                if (this.priority> node.getPriority()){
+                    return -1;
+                } else if (this.priority< node.getPriority()){
+                    return 1;
+                } else{
+                    return 0;
+                }
+
+            }
         }
     }
 
